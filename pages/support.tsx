18a47e15f794e19345ce0279e96b5cfc53ca111a
BrianSayen <blu3fire89@gmail.com>
2022-08-26 13:13:37 +0800

    fix: ts types error

--- pages/support.tsx
+++ pages/support.tsx
@@ -1,24 +1,40 @@
 import { useEffect, useState } from 'react'
-import io from 'socket.io-client';
+import { io, Socket } from 'socket.io-client';
 import type { GetStaticProps, NextPage } from 'next'
-
+import {ServerToClientEvents, ClientToServerEvents, Slot} from '../types'
 const server = 'https://prs.uat.api.xyphersolutionsinc.com'
 // const server = 'http://localhost:5080'
-const socket = io(server, {transports: ['websocket']})
+// const socket = io(server, {transports: ['websocket']})
+
+function useSocket(url:string) {
+  const [socket, setSocket] = useState<Socket<ServerToClientEvents> | undefined>()
+
+  useEffect(() => {
+    const socketIo: Socket<ServerToClientEvents> = io(url, {transports: ['websocket']})
+    setSocket(socketIo)
+
+    function cleanup() {
+      socketIo.disconnect()
+    }
+    return cleanup
+  }, [])
+
+  return socket
+}
 
 const support = ({slots}: any) => {
   const [availSlot, setSlotAvail] = useState<number>(slots.data.slotsAvailable)
-  // console.log('socket', socket)
-  // console.log('slots', slots)
-  socket.on("connection", (data) => {
-    const engine = socket.io.engine;
-    console.log(engine.transport.name);
-    // console.log('data', data)
-    // setSlotAvail(data.slots.slotsAvailable)
-  })
-  const handleAdd = () => {
 
-  }
+  const socket = useSocket(server)
+  useEffect(() => {
+    function handleEvent(prop: {slots:Slot}) {
+      console.log('slots', prop.slots.bookingInSlot)
+      setSlotAvail(prop.slots.slotsAvailable)
+    }
+    if (socket) {
+      socket.on("update-slot", handleEvent)
+    }
+  }, [socket])
 
   return (
     <div>
@@ -33,15 +49,7 @@ const support = ({slots}: any) => {
 }
 
 export const getStaticProps: GetStaticProps = async(context) => {
-  // const res = await fetch("https://fakestoreapi.com/products/categories")
   const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzMDViNzIwMGEzZjQ2MWI3OTIwNmIxYiIsImlhdCI6MTY2MTMyOTQyMH0.ZxyeVu7XNzbZweiQ-HDVMo5NYXVGw_QR59ER8z3eSW4';
-  // fetch('https://prs.uat.api.xyphersolutionsinc.com/dashboard/slots', { 
-  //   method: 'get', 
-  //   headers: {
-  //     'Content-type': 'application/json',
-  //     'Authorization': `Bearer ${token}`, // notice the Bearer before your token
-  //   },
-  // });
   const res = await fetch(`${server}/dashboard/slots`, { 
     method: 'get', 
     headers: {
--- types.ts
+++ types.ts
@@ -31,3 +31,34 @@ export interface CategoriesProps {
 export interface CategoryProps {
   category: string;
 }
+
+export interface ServerToClientEvents {
+  noArg: () => void;
+  basicEmit: (a: number, b: string, c: Buffer) => void;
+  withAck: (d: string, callback: (e: number) => void) => void;
+  "update-slot": (prop: {slots:Slot}) => void
+}
+
+export interface ClientToServerEvents {
+  hello: () => void;
+}
+
+export interface InterServerEvents {
+  ping: () => void;
+}
+
+export interface SocketData {
+  name: string;
+  age: number;
+}
+
+export interface Slot {
+  bookingInSlot: number;
+  bookingReservedSlot: number;
+  maxCapacity: number;
+  onlineBookingOccupied: number;
+  slotsAvailable: number;
+  totalBooking: number;
+  totalOccupied: number;
+  walkInSlot: number;
+}
